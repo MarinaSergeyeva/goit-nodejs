0f41b6ee0666496c811836666498afa122e58231
Marina Sergeyeva <morrrinad@gmail.com>
2020-10-02 20:06:02 +0300

    commands added

--- index.js
+++ index.js
@@ -8,18 +8,22 @@ async function invokeAction({ action, id, name, email, phone }) {
   switch (action) {
     case "list":
       console.table(await contacts.listContacts());
+      //   node index.js --action="list"
       break;
 
     case "get":
       console.table(await contacts.getContactById(id));
+      //   node index.js --action="get" --id=5
       break;
 
     case "add":
       console.table(await contacts.addContact(name, email, phone));
+      //   node index.js --action="add" --name="Mango" --email="mango@gmail.com" --phone="322-22-22"
       break;
 
     case "remove":
       console.table(await contacts.removeContact(id));
+      //   node index.js --action="remove" --id=3
       break;
 
     default:
