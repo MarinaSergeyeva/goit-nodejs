9fdd86da557fa07246dda5b599e1e7dc9c62d58f
Marina Sergeyeva <morrrinad@gmail.com>
2020-11-09 23:24:21 +0200

    error message fixed

--- src/api/emailSender/emailSender.js
+++ src/api/emailSender/emailSender.js
@@ -2,27 +2,29 @@ const path = require('path');
 require('dotenv').config({ path: '.env' });
 const sgMail = require('@sendgrid/mail');
 const catchAsync = require('../../utils/catchAsync');
+const AppError = require('../errors/appError');
 
 const SENDGRID_API_KEY = process.env.SENDGRID_API_KEY;
 sgMail.setApiKey(SENDGRID_API_KEY);
 
+const sendMessage = catchAsync(async msg => {
+  const email = await sgMail.send(msg);
+
+  if (!email) {
+    return next(new AppError('Not Found'), 404);
+  }
+});
+
 const verificationSender = catchAsync(async (email, verificationToken) => {
   const msg = {
     to: email, // Change to your recipient
-    from: process.env.SENDGRID_SENSER, // Change to your verified sender
+    from: process.env.SENDGRID_SENDER, // Change to your verified sender
     subject: 'Sendgrid Node.js Homework',
     text: 'HW-06 Verification Sender',
     html: `<p> Please verify your account and follow this <a href='http://localhost:3000/api/v1/auth/verify/${verificationToken}'>link</a>`,
   };
 
-  await sgMail
-    .send(msg)
-    .then(() => {
-      console.log('Email sent');
-    })
-    .catch(error => {
-      console.error(error);
-    });
+  sendMessage(msg);
 });
 
 module.exports = {
